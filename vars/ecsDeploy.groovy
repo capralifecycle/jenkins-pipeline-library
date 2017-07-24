5b48b099b8a36f0cc5fae3a68dcd561921c69199
Henrik Steen <henrist@henrist.net>
2017-07-24 07:14:36 +0200

    ecsDeploy: Fix arg name

--- vars/ecsDeploy.groovy
+++ vars/ecsDeploy.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/groovy
 
-def call(body) {
+def call(args) {
   // AWS_CONTAINER_CREDENTIALS_RELATIVE_URI contains a variable
   // used by awscli to pick up the ECS task role instead of using
   // instance role. This lets us use the task role of the jenkins slave.
