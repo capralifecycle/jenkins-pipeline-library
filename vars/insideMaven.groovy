3df2bf8e966b9fd8cf9823f48b5175131b126b16
Henrik Steen <henrist@henrist.net>
2017-07-28 14:04:48 +0200

    Add insideMaven method

---
+++ vars/insideMaven.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/groovy
+
+/**
+ * Run inside a Maven container
+ *
+ * See withMavenSettings for how to use Maven settings
+ */
+def call(body) {
+  docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/maven:3-alpine').inside('-v cals-m2-cache:/home/jenkins/.m2') {
+    withMavenSettings {
+      body()
+    }
+  }
+}
