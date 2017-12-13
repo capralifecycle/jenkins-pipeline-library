e5283a3ddc46f3a5f4c4526be0ea3386f7839afc
Anders Rønning <andeero@ifi.uio.no>
2017-12-13 12:45:17 +0100

    WIP serverless deploy

--- vars/serverlessDeploy.groovy
+++ vars/serverlessDeploy.groovy
@@ -7,13 +7,12 @@ def call(args) {
   // AWS_CONTAINER_CREDENTIALS_RELATIVE_URI contains a variable
   // used by awscli to pick up the ECS task role instead of using
   // instance role. This lets us use the task role of the jenkins slave.
-  docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/serverless').inside("-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=${env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}") {
+  docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/serverless').inside("-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=${env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI} -u root") {
     def accessKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .AccessKeyId', returnStdout: true)
     def secretKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .SecretAccessKey', returnStdout: true)
     sh "serverless config credentials --provider aws --key $accessKey --secret $secretKey"
     sh "ls -al"
     sh "pwd"
-    sh "$USER"
     sh "serverless deploy $args"
   }
 }
