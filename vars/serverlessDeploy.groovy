ff9917bb54d8150cd75f6c59a4e1f5ef8b926335
Anders Rønning <andeero@ifi.uio.no>
2017-12-14 13:49:54 +0100

    WIP serverless-deploy

--- vars/serverlessDeploy.groovy
+++ vars/serverlessDeploy.groovy
@@ -8,10 +8,11 @@ def call(args) {
   // used by awscli to pick up the ECS task role instead of using
   // instance role. This lets us use the task role of the jenkins slave.
   docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/serverless').inside("-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=${env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}") {
-    def accessKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .AccessKeyId', returnStdout: true).trim()
-    def secretKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .SecretAccessKey', returnStdout: true).trim()
+//    def accessKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .AccessKeyId', returnStdout: true).trim()
+//    def secretKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .SecretAccessKey', returnStdout: true).trim()
     sh '''
       #!/bin/bash
+      set +x
       
       CREDS=$(aws sts assume-role --role-arn \\
         arn:aws:iam::644399498992:role/ServerlessDeployBot \\
@@ -19,12 +20,14 @@ def call(args) {
       export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.Credentials.AccessKeyId')
       export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey')
       export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
-      
-      echo $@
-      $@
     '''
 
-    sh "#!/bin/sh -e\\n export HOME=\$(pwd); serverless config credentials --provider aws --key \$AWS_ACCESS_KEY --secret \$AWS_SECRET_ACCESS_KEY" //do not output credentials
-    sh "serverless deploy $args"
+    sh '''
+      #!/bin/sh
+      set +x
+      
+      export HOME=$(pwd); serverless config credentials --provider aws --key $AWS_ACCESS_KEY --secret $AWS_SECRET_ACCESS_KEY
+      sh "serverless deploy $args"
+    '''
   }
 }
