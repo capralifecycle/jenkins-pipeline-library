e65883d86c4bc2e959ad0fc7c8185489970bbb5e
Anders Rønning <andeero@ifi.uio.no>
2017-12-14 12:42:46 +0100

    WIP serverless-deploy

--- vars/serverlessDeploy.groovy
+++ vars/serverlessDeploy.groovy
@@ -7,10 +7,24 @@ def call(args) {
   // AWS_CONTAINER_CREDENTIALS_RELATIVE_URI contains a variable
   // used by awscli to pick up the ECS task role instead of using
   // instance role. This lets us use the task role of the jenkins slave.
-  docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/serverless').inside("-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=${env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI} -u root") {
+  docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/serverless').inside("-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=${env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}") {
     def accessKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .AccessKeyId', returnStdout: true).trim()
     def secretKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .SecretAccessKey', returnStdout: true).trim()
-    sh "#!/bin/sh -e\\n export HOME=\$(pwd); serverless config credentials --provider aws --key ${accessKey} --secret ${secretKey}" //do not output credentials
+    sh '''
+      #!/bin/bash
+      
+      CREDS=$(aws sts assume-role --role-arn \\
+        arn:aws:iam::644399498992:role/ServerlessDeployBot \\
+        --role-session-name serverless-deploy-session --out json)
+      export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.Credentials.AccessKeyId')
+      export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey')
+      export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
+      
+      echo $@
+      $@
+    '''
+
+    sh "#!/bin/sh -e\\n export HOME=\$(pwd); serverless config credentials --provider aws --key \$AWS_ACCESS_KEY --secret \$AWS_SECRET_ACCESS_KEY" //do not output credentials
     sh "serverless deploy $args"
   }
 }
