b9652665a57ff99ba524ed1529520593a0826ff3
Anders Rønning <andeero@ifi.uio.no>
2017-12-14 14:02:17 +0100

    WIP serverless-deploy

--- vars/serverlessDeploy.groovy
+++ vars/serverlessDeploy.groovy
@@ -8,11 +8,8 @@ def call(args) {
   // used by awscli to pick up the ECS task role instead of using
   // instance role. This lets us use the task role of the jenkins slave.
   docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/serverless').inside("-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=${env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}") {
-//    def accessKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .AccessKeyId', returnStdout: true).trim()
-//    def secretKey = sh(script: 'curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI | jq .SecretAccessKey', returnStdout: true).trim()
     sh '''
       #!/bin/bash
-      set +x
       
       CREDS=$(aws sts assume-role --role-arn \\
         arn:aws:iam::644399498992:role/ServerlessDeployBot \\
@@ -20,12 +17,8 @@ def call(args) {
       export AWS_ACCESS_KEY_ID=$(echo $CREDS | jq -r '.Credentials.AccessKeyId')
       export AWS_SECRET_ACCESS_KEY=$(echo $CREDS | jq -r '.Credentials.SecretAccessKey')
       export AWS_SESSION_TOKEN=$(echo $CREDS | jq -r '.Credentials.SessionToken')
-    '''
-
-    sh '''
-      #!/bin/sh
-      set +x
       
+      # Must set HOME as it is not set and thus serverless will default to root dir which the user does not have read/write access to
       export HOME=$(pwd); serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
       sh "serverless deploy $args"
     '''
