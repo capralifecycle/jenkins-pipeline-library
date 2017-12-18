e115dd13ff5003662bc799664f5a312b11cc4a8b
Anders Rønning <andeero@ifi.uio.no>
2017-12-18 17:43:59 +0100

    WIP serverless-deploy

--- vars/serverlessDeploy.groovy
+++ vars/serverlessDeploy.groovy
@@ -8,6 +8,7 @@ def call(deployIamRole, serverlessArgs) {
   // used by awscli to pick up the ECS task role instead of using
   // instance role. This lets us use the task role of the jenkins slave.
   docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/serverless').inside("-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI=${env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}") {
+    sh "${serverlessArgs}"
     sh '''
       #!/bin/bash
       set +x
