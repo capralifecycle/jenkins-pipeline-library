01acc9cfb7ab129b3ba7b038130fd1b08f5b5505
Anders Rønning <andeero@ifi.uio.no>
2017-12-18 18:00:50 +0100

    WIP serverless-deploy

--- vars/serverlessDeploy.groovy
+++ vars/serverlessDeploy.groovy
@@ -22,6 +22,6 @@ def call(deployIamRole, serverlessArgs) {
       
       # Must set HOME as it is not set and thus serverless will default to root dir which the user does not have read/write access to
       export HOME=$(pwd); serverless config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
-      serverless deploy ''' + serverlessArgs
+      serverless deploy ''' + serverlessArgs + ''' '''
   }
 }
