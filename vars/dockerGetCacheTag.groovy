3a2a26b6da2c8a50a09b0f5ecbaafe6722ddbf72
Henrik Steen <henrist@henrist.net>
2018-06-27 23:31:26 +0200

    Support caching on non-multi-branch pipelines and fix initial caching

--- vars/dockerGetCacheTag.groovy
+++ vars/dockerGetCacheTag.groovy
@@ -1,9 +1,13 @@
 #!/usr/bin/groovy
 
 def call(suffix = null) {
-  // Avoid possible injections/errors by whitelisting branch name contents
-  // The return value is shell-safe
-  def tag = "ci-cache-$BRANCH_NAME".replaceAll(/[^a-zA-Z0-9\-_]/, '')
+  def tag = "ci-cache"
+
+  if (env.BRANCH_NAME != null) {
+    // Avoid possible injections/errors by whitelisting branch name contents
+    // The return value is shell-safe
+    tag += "-" + env.BRANCH_NAME.replaceAll(/[^a-zA-Z0-9\-_]/, '')
+  }
 
   if (suffix != null) {
     tag += '-' + suffix
