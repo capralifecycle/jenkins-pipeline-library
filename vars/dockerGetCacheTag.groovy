b54449902c6f5a974ed4b61dee3c1aea9c639e65
Henrik Steen <henrist@henrist.net>
2018-06-27 19:51:12 +0200

    Support multiple docker caches for the same branch

--- vars/dockerGetCacheTag.groovy
+++ vars/dockerGetCacheTag.groovy
@@ -1,7 +1,13 @@
 #!/usr/bin/groovy
 
-def call(body) {
+def call(suffix = null) {
   // Avoid possible injections/errors by whitelisting branch name contents
   // The return value is shell-safe
-  return "ci-cache-$BRANCH_NAME".replaceAll(/[^a-zA-Z0-9\-_]/, '')
+  def tag = "ci-cache-$BRANCH_NAME".replaceAll(/[^a-zA-Z0-9\-_]/, '')
+
+  if (suffix != null) {
+    tag += '-' + suffix
+  }
+
+  return tag
 }
