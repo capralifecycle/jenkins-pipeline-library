74bc274fa42be303eab7d3c26f58b85d207fde73
Henrik Steen <henrist@henrist.net>
2018-07-14 03:29:13 +0200

    insideMaven: Add parameter to avoid alpine version

--- vars/insideMaven.groovy
+++ vars/insideMaven.groovy
@@ -5,8 +5,17 @@
  *
  * See withMavenSettings for how to use Maven settings
  */
-def call(body) {
-  docker.image('923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/tool/maven:3-alpine').inside('-v cals-m2-cache:/home/jenkins/.m2') {
+def call(Map args = [:], body) {
+  def tool = args.avoidAlpine == null || !args.avoidAlpine
+    ? 'maven:3-alpine'
+    : 'maven-debian:3.5.2-slim'
+
+  def insideArgs = '-v cals-m2-cache:/home/jenkins/.m2'
+  if (args.insideArgs != null) {
+    insideArgs += ' ' + args.insideArgs
+  }
+
+  docker.image("923402097046.dkr.ecr.eu-central-1.amazonaws.com/buildtools/tool/$tool").inside(insideArgs) {
     withMavenSettings {
       body()
     }
