902d14c834827160e29912dafec58e7098855b1a
Henrik Steen <henrist@henrist.net>
2019-07-09 01:52:08 +0200

    insideMaven: Add version arg

--- vars/insideMaven.groovy
+++ vars/insideMaven.groovy
@@ -6,7 +6,11 @@
  * See withMavenSettings for how to use Maven settings
  */
 def call(Map args = [:], body) {
-  def tool = args.avoidAlpine == null || !args.avoidAlpine
+  // Prefer the use of providing the version, e.g.
+  // [ version: '3-jdk-11-alpine' ]
+  def tool = args.version != null
+    ? "maven:${args.version}"
+    : args.avoidAlpine == null || !args.avoidAlpine
     ? 'maven:3-jdk-8-alpine'
     : 'maven:3-jdk-8-debian'
 
