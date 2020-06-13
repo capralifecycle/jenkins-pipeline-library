34107df42ea9d58780e84586364e065c685effce
Henrik Steen <henrist@henrist.net>
2020-06-13 11:34:07 +0200

    Pass AWS_CONTAINER_CREDENTIALS_RELATIVE_URI for insideToolImage

--- vars/insideToolImage.groovy
+++ vars/insideToolImage.groovy
@@ -6,7 +6,13 @@
  * The provided name can include a tag, e.g. node:12-alpine
  */
 def call(name, Map args = [:], body) {
-  def insideArgs = args.insideArgs ?: ''
+  // Passing AWS_CONTAINER_CREDENTIALS_RELATIVE_URI allows the processes
+  // to access the IAM role for the Jenkins slave, similar as processes
+  // running outside the tool image.
+  def insideArgs = "-e AWS_CONTAINER_CREDENTIALS_RELATIVE_URI"
+  if (args.insideArgs) {
+    insideArgs += " " + args.insideArgs
+  }
 
   def img = docker.image(toolImageDockerReference(name))
   img.pull() // ensure we have latest version
