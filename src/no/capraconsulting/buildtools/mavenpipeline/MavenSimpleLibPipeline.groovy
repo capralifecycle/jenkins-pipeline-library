5560491bb93483a9e1ba2b4308a8f21256aaf680
Trond Engell <trond.engell@gmail.com>
2020-08-31 12:30:06 +0200

    Initial commit for mvn simple lib pipeline (WIP)

---
+++ src/no/capraconsulting/buildtools/mavenpipeline/MavenSimpleLibPipeline.groovy
@@ -0,0 +1,168 @@
+#!/usr/bin/groovy
+package no.capraconsulting.buildtools.maven
+
+import no.capraconsulting.buildtools.maven.ConfigDelegate
+import no.capraconsulting.buildtools.maven.CreateBuildDelegate
+
+import static no.capraconsulting.buildtools.maven.MavenPipeline.checkNotNull as checkNotNull
+
+
+//@Library('cals') _
+//
+//import no.capraconsulting.buildtools.mavenpipeline.MavenPipeline
+//
+//def p = new MavenPipeline()
+//p.pipeline {
+//  build = p.createBuild {
+//    dockerBuildImage = toolImageDockerReference('maven:3-jdk-8-alpine')
+//  }
+//}
+
+
+def simpleLibPipeline(Closure cl) {
+  def config = new ConfigDelegate()
+  cl.resolveStrategy = Closure.DELEGATE_FIRST
+  cl.delegate = config
+  cl()
+
+  checkNotNull(config.build, 'build')
+
+  buildConfig(config.buildConfigParams) {
+    config.build.call(config)
+  }
+}
+//void checkNotNull(value, name) {
+//  if (value == null) {
+//    throw Exception("$name must be set")
+//  }
+//}
+
+def createBuild(Closure cl) {
+  def buildConfig = new CreateBuildDelegate()
+  cl.resolveStrategy = Closure.DELEGATE_FIRST
+  cl.delegate = buildConfig
+  cl()
+
+  // Verify build config requirements that must be set/changed.
+  checkNotNull(buildConfig.dockerBuildImage, 'dockerBuildImage')
+
+  def build = { config ->
+    dockerNode([label: buildConfig.dockerNodeLabel]) {
+      def buildImage = docker.image(buildConfig.dockerBuildImage)
+      buildImage.pull() // Ensure latest version
+
+      buildImage.inside {
+        stage('Checkout source') {
+          checkout scm
+        }
+
+        try {
+          stage('Build and conditionally release') {
+            withMavenDeployVersionByTimeEnv { String revision ->
+              String goal = env.BRANCH_NAME == "master" && changedSinceLatestTag()
+                ? "source:jar deploy scm:tag"
+                : "verify"
+              sh """
+                        mvn -s \$MAVEN_SETTINGS org.apache.maven.plugins:maven-enforcer-plugin:3.0.0-M3:enforce -Drules=requireReleaseDeps
+                        mvn -s \$MAVEN_SETTINGS -B -Dtag=$revision -Drevision=$revision $goal
+                    """
+            }
+          }
+        } finally {
+//          saveJunitReport()
+//          saveJacocoReport()
+        }
+      }
+
+      if (buildConfig.dockerBuild) {
+        buildConfig.dockerBuild.call()
+      }
+    }
+  }
+
+  return build
+}
+
+//class CreateBuildDelegate implements Serializable {
+//  /** Optional override of Jenkins slave node label. */
+//  String dockerNodeLabel
+//  String dockerBuildImage
+//  /** Optional extra args to mvn command. */
+//  String mavenArgs = ''
+//  /** The goal targeted. */
+//  String mavenGoals = 'verify'
+//  /**
+//   * Docker build process. Most of our services we build with Maven
+//   * builds a Docker image that will be run in an environment.
+//   * Optional.
+//   */
+//  Closure dockerBuild
+//}
+
+private Boolean changedSinceLatestTag() {
+  String currentCommit = headCommitHash()
+  String previousTag = previousTagLabel()
+  String commitOfPreviousTag = underlyingCommit(previousTag)
+  echo "CurrentCommit: ${currentCommit}"
+  echo "PreviousTag: ${previousTag}"
+  echo "PreviousTagCommit: ${commitOfPreviousTag}"
+
+  def hasChange = currentCommit != commitOfPreviousTag
+  if (hasChange)
+    echo "Build has change."
+  else
+    echo "Build has no change."
+  hasChange
+}
+
+/**
+ * @return Underlying commit hash for tag.
+ */
+private String underlyingCommit(String tagLabel) {
+  sh(
+    script: "git rev-parse $tagLabel^{}",
+    returnStdout: true
+  ).trim()
+}
+
+/**
+ * @return Tag label for latest pushed tag in current branch.
+ */
+private String previousTagLabel() {
+  sh "git fetch --tags"
+  sh(
+    script: "git describe --abbrev=0",
+    returnStdout: true
+  ).trim()
+}
+
+/**
+ * @return Head commit hash.
+ */
+private String headCommitHash() {
+  sh(
+    script: "git rev-parse HEAD",
+    returnStdout: true
+  ).trim()
+}
+
+def withMavenDeployVersionByTimeEnv(body) {
+  withGitConfig {
+    withMavenSettings {
+      String majorVersion = readMavenPom().getProperties()['major-version']
+      String revision = revision(majorVersion)
+
+      withEnv(["revision=$revision"]) {
+        body(revision)
+      }
+    }
+  }
+}
+
+private static String revision(String majorVersion) {
+  def now = new Date()
+  def buildDate = now.format("yyyyMMdd", TimeZone.getTimeZone("UTC"))
+  def buildTime = now.format("HHmmss", TimeZone.getTimeZone("UTC"))
+  "$majorVersion.$buildDate.$buildTime"
+}
+
