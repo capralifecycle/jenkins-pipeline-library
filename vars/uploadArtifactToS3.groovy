4a22399a1d965f4fbf8325a485003f4b63e1cd23
Trond Engell <trond.engell@gmail.com>
2021-07-05 13:44:17 +0200

    Simplify logic

--- vars/uploadArtifactToS3.groovy
+++ vars/uploadArtifactToS3.groovy
@@ -5,35 +5,19 @@
  * bucket name using the given IAM role.
  */
 def call(Map config) {
-  def artifactPrefix = require(config, "artifactPrefix")
+  def artifactFileName = require(config, "artifactPrefix")
   def artifactDir = require(config, "artifactDir")
-  def artifactsBucketName = require(config, "artifactsBucketName")
-  def artifactsRoleArn = require(config, "artifactsRoleArn")
+
+  def bucketName = require(config, "artifactsBucketName")
+  def bucketDir = require(config, "bucketDir")
+  def roleArn = require(config, "artifactsRoleArn")
 
   dir(artifactDir) {
-    def fileName = sh """
-      find . -type f -iname "$artifactPrefix*" -exec basename {} \\;
-    """
+    def s3Key = "$bucketDir/$artifactFileName"
+    def s3Url = "s3://$bucketName/$s3Key"
 
-    def fileExtension = sh """
-      "\${$fileName##*.}"
-    """
-
-    // TODO: The file will include timestamps causing new hash to
-    //  be created even when it has the same file contents.
-    //  Consider reworking this so we can produce deterministic zip
-    //  files for the same content excluding timestamps.
-
-    def sha256 = sh([
-      returnStdout: true,
-      script: "sha256sum ./$fileName | awk '{print \$1}'"
-    ]).trim()
-
-    def s3Key = "builds/${sha256}.$fileExtension"
-    def s3Url = "s3://$artifactsBucketName/$s3Key"
-
-    withAwsRole(artifactsRoleArn) {
-      sh "aws s3 cp /$artifactDir/$fileName $s3Url"
+    withAwsRole(roleArn) {
+      sh "aws s3 cp /$artifactDir/$artifactFileName $s3Url"
     }
 
     s3Key
