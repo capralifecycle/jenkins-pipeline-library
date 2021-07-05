07a830a61e51c56ed3e34f6eb67ec5a4075e3d64
Trond Engell <trond.engell@gmail.com>
2021-07-05 13:16:28 +0200

    Upload artifact to s3

---
+++ vars/uploadArtifactToS3.groovy
@@ -0,0 +1,44 @@
+#!/usr/bin/groovy
+
+/**
+ * Upload jar-artifact to S3 to the given
+ * bucket name using the given IAM role.
+ */
+def call(Map config) {
+  def artifactPrefix = require(config, "artifactPrefix")
+  def artifactDir = require(config, "artifactDir")
+  def artifactsBucketName = require(config, "artifactsBucketName")
+  def artifactsRoleArn = require(config, "artifactsRoleArn")
+
+  dir(artifactDir) {
+    def artifactFileName = sh """
+      find . -type f -iname "$artifactPrefix*" -exec basename {} \\;
+    """
+
+    // TODO: The file will include timestamps causing new hash to
+    //  be created even when it has the same file contents.
+    //  Consider reworking this so we can produce deterministic zip
+    //  files for the same content excluding timestamps.
+
+    def sha256 = sh([
+      returnStdout: true,
+      script: "sha256sum ./$artifactFileName | awk '{print \$1}'"
+    ]).trim()
+
+    def s3Key = "builds/${sha256}.jar" // TODO: Is filetype needed?
+    def s3Url = "s3://$artifactsBucketName/$s3Key"
+
+    withAwsRole(artifactsRoleArn) {
+      sh "aws s3 cp /$artifactDir/$artifactFileName $s3Url"
+    }
+
+    s3Key
+  }
+}
+
+private def require(Map config, String name) {
+  if (!config.containsKey(name)) {
+    throw new Exception("Missing $name")
+  }
+  return config[name]
+}
