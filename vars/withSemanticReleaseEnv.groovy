0420cf41d90c52fb6caa635c81dbf5f240486b63
Erlend Ekern <stekern@users.noreply.github.com>
2023-04-17 08:03:54 +0200

    feat: add authentication for github packages npm registry (#38)

--- vars/withSemanticReleaseEnv.groovy
+++ vars/withSemanticReleaseEnv.groovy
@@ -2,17 +2,13 @@
 
 /**
  * Provide neccessary configuration to give semantic-release
- * credentials to perform Git push, npmjs release and GitHub release.
+ * credentials to perform Git push, npmjs release, GitHub release and GitHub Packages release.
  *
  * For more details:
  * https://semantic-release.gitbook.io/semantic-release/usage/ci-configuration#authentication
  */
 def call(body) {
   withCredentials([
-    string(
-      credentialsId: 'npmjs-capraconsulting-deploy-token',
-      variable: 'NPM_TOKEN',
-    ),
     usernamePassword(
       credentialsId: 'github-calsci-token-with-user',
       passwordVariable: 'GH_TOKEN',
@@ -20,7 +16,20 @@ def call(body) {
     ),
   ]) {
     withEnv(['CI=true']) {
-      body()
+     /**
+      * Add a configuration file for NPM providing authentication details to
+      * the GitHub Packages NPM registry and npmjs. The location of the configuration file is stored in
+      * the NPM_CONFIG_USERCONFIG variable which is automatically picked up by NPM.
+      *
+      * The config file github-packages-npm-settings is set up in Jenkins
+      * by using config-file-provider plugin and uses credentials
+      * from the credentials store in Jenkins.
+      */
+      configFileProvider([
+        configFile(fileId: 'github-packages-npm-settings', variable: 'NPM_CONFIG_USERCONFIG'),
+      ]) {
+        body()
+      }
     }
   }
 }
